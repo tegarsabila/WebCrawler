720aa0de3c9a3da0c32f4f6249493dc583d8674b
Tegar Sabila <tegarsabila11@gmail>
2024-01-29 05:29:17 +0700

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,35 @@ function startCrawling() {
     var website = document.getElementById("website").value;
     document.getElementById("output").innerHTML = "Crawling... Please wait.";
 
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(website)}`)
-        .then(response => response.json())
+    fetchPageContent(website)
         .then(data => {
             var parser = new DOMParser();
-            var doc = parser.parseFromString(data.contents, 'text/html');
-            var links = Array.from(doc.querySelectorAll('a')).map(a => a.href);
+            var doc = parser.parseFromString(data, 'text/html');
+            var links = Array.from(doc.querySelectorAll('a')).map(a => resolveRelativeURL(a.href, website));
 
             document.getElementById("output").innerHTML = formatOutput(links);
         })
         .catch(error => console.error('Error:', error));
+}
+
+async function fetchPageContent(url) {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+}
+
+function resolveRelativeURL(href, base) {
+    var doc = document.implementation.createHTMLDocument();
+    var baseElement = doc.createElement('base');
+    baseElement.href = base;
+    doc.head.appendChild(baseElement);
+    var a = doc.createElement('a');
+    console.log(a)
+    a.href = href;
+    const regexSearch = /https:\/\/tegarsabila\.github\.io/gi;
+    const replacedString = a.href.replace(regexSearch, base)
+    return replacedString
+    
 }
 
 function formatOutput(links) {
